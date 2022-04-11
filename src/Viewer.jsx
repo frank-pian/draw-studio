84856f8d1fad772992436650c91de152dac0ca8d
bianyongfang <bianyongfang@vip.qq.com>
2022-04-11 14:41:50 +0800

    add font

--- src/Viewer.jsx
+++ src/Viewer.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class Viewer extends Component {
     render() {
-        return <canvas id="occViewerCanvas" style={{ width: "100%", height: "100%", border: "0 none", backgroundColor: "#000" }} />;
+        return (
+            <canvas id="occViewerCanvas" style={{ width: "100%", height: "100%", border: "0 none", backgroundColor: "#000" }} />
+        );
     }
 }
 
